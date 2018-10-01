f3b781379ff23088bfe0dbcbdf9de386bbb544f8
Jay Mistry <mistry.jay15@gmail.com>
2018-10-01 23:37:11 +0530

    a

--- MARCH18B/BIGSALE.cpp
+++ MARCH18B/BIGSALE.cpp
@@ -13,13 +13,13 @@ int main() {
     for (int q = 0; q < t; q++) {
         int n;
         cin>>n;
-        double loss=0;
-        for(int i=0;i<n;i++){
-            double p,u,d;
-            cin>>p>>u>>d;
+        double loss = 0;
+        for(int i=0 ; i<n ; i++) {
+            double p, u, d;
+            cin >> p >> u >> d;
             loss += (u*p*d*d/10000.000000000);
         }
-        cout<<std::setprecision(100)<<loss<<endl;
+        cout << std::setprecision(100) << loss << endl;
  
     }
 } 
