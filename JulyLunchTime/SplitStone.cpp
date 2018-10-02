c8595d646b7ce783fb0ee0be4412594a3714a81c
Shashi Kumar fagna <39130555+Plenue@users.noreply.github.com>
2018-10-02 14:25:30 +0530

    Update SplitStone.cpp

--- JulyLunchTime/SplitStone.cpp
+++ JulyLunchTime/SplitStone.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int main() {
     ll t;
     cin>>t;
-    while(t--){
+    while(t--)
+    {
         ll a,b,c,x,y;
         cin>>a>>b>>c>>x>>y;
-        if(a+b+c==x+y&&min(a,min(c,b))<=min(x,y)){
+        if(a+b+c==x+y&&min(a,min(c,b))<=min(x,y))
             cout<<"YES\n";
-        }
+    }
         else cout<<"NO\n";
     }
 	return 0;
