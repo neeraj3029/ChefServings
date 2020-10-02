2812e5d1978ae2b58cc282bc709cffb5d1f4e234
Pawan Kushwaha <55081234+pkpawankushwaha@users.noreply.github.com>
2020-10-02 14:00:36 +0530

    create 1393A.cpp

---
+++ CF/662Div2/1393A.cpp
@@ -0,0 +1,36 @@
+// Link for the problem     https://codeforces.com/contest/1393/problem/A
+
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+#define ll long long int
+#define tc ll tetra;cin>>tetra;while(tetra--)
+#define lp(i,j,k) for(ll i=j;i<k;i++)
+#define lpr(i,j,k) for(ll i=j;i>=k;i--)
+#define F first
+#define S second
+#define mod 1000000007
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define vec(pk,pk1) vector<ll> pk(pk1,0)
+#define vect(mc) vector<ll> mc
+#define vpair(love_pair) vector<pair<ll,ll> > love_pair
+#define pb(x) push_back(x)
+#define LB(tector,search) lower_bound(tector.begin(),tector.end(),search)
+#define UB(tector,search) upper_bound(tector.begin(),tector.end(),search)
+#define all(lol) lol.begin(),lol.end()
+#define fileinput; freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+int main()
+{
+	fast
+	
+	tc{
+	
+	ll n;cin>>n;
+	if(n==1)cout<<"1"<<endl;
+	else if(n==2)cout<<"2"<<endl;
+	
+	else{
+		cout<<(n/2+1)<<endl;
+	}
+	}
+}
